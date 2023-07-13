2f5df1e28640392a1070603d26727659cc30d357
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-13 17:38:33 +0530

    [Riya] Add : Implemented LeapYear

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class LeapYear
+    {
+        public void CheckLeapYear()
+        {
+            Console.WriteLine("Enter the year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                Console.WriteLine("Leap year");
+            else
+                Console.WriteLine("Not a leap year");
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,8 +6,26 @@ namespace BasicCorePrograms
     {
         static void Main(string[] args)
         {
-            Flipcoin flipcoin = new Flipcoin();
-            flipcoin.Play();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter input to be executed\n 1.Flip Coin\n 2.Leap Year\n 3.Exit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Flipcoin flipcoin = new Flipcoin();
+                        flipcoin.Play();
+                        break;
+                    case 2:
+                        LeapYear leapYear = new LeapYear();
+                        leapYear.CheckLeapYear();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
